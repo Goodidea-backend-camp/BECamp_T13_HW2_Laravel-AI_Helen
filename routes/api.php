2cb2a230b8054a9c5f4ef2d96c343dfe2a251e6f
lovepp0518 <lovepp0518@gmail.com>
2024-09-15 11:52:53 +0800

    refactor: group authenticated routes for cleaner routes structure

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,18 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatMessageController;
 use App\Http\Controllers\ThreadController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware(['auth:sanctum', 'can:create,App\Models\ChatMessage,threadId'])->post('/threads/{threadId}/messages/text', [ChatMessageController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->post('/threads', [ThreadController::class, 'store']);
-Route::middleware(['auth:sanctum', 'can:update,thread'])->put('/threads/{thread}', [ThreadController::class, 'update']);
-Route::middleware(['auth:sanctum', 'can:delete,thread'])->delete('/threads/{thread}', [ThreadController::class, 'destroy']);
+    Route::post('/threads', [ThreadController::class, 'store']);
+    Route::put('/threads/{thread}', [ThreadController::class, 'update'])->middleware('can:update,thread');
+    Route::delete('/threads/{thread}', [ThreadController::class, 'destroy'])->middleware('can:delete,thread');
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::post('/threads/{threadId}/messages/text', [ChatMessageController::class, 'store'])
+        ->middleware('can:create,App\Models\ChatMessage,threadId');
+});
